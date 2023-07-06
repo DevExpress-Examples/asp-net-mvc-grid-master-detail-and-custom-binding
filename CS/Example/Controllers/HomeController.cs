7e0eba98a1dc2f73edf42c2d414fb2ec7e6ad0ac
nikulina.svetlana <svetlana.nikulina@devexpress.com>
2023-07-06 13:27:40 +0400

    format code

--- CS/Example/Controllers/HomeController.cs
+++ CS/Example/Controllers/HomeController.cs
@@ -7,31 +7,26 @@ namespace Example.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
             return View();
         }
-        public ActionResult MasterGridViewPartial()
-        {
+        public ActionResult MasterGridViewPartial() {
             var viewModel = GridViewExtension.GetViewModel("masterGrid");
             if (viewModel == null)
                 viewModel = CreateMasterGridViewModel();
             return MasterGridActionCore(viewModel);
         }
-        public ActionResult MasterGridViewSortingAction(GridViewColumnState column, bool reset)
-        {
+        public ActionResult MasterGridViewSortingAction(GridViewColumnState column, bool reset) {
             var viewModel = GridViewExtension.GetViewModel("masterGrid");
             viewModel.SortBy(column, reset);
             return MasterGridActionCore(viewModel);
         }
-        public ActionResult MasterGridViewPagingAction(GridViewPagerState pager)
-        {
+        public ActionResult MasterGridViewPagingAction(GridViewPagerState pager) {
             var viewModel = GridViewExtension.GetViewModel("masterGrid");
             viewModel.Pager.Assign(pager);
             return MasterGridActionCore(viewModel);
         }
-        public ActionResult MasterGridActionCore(GridViewModel gridViewModel)
-        {
+        public ActionResult MasterGridActionCore(GridViewModel gridViewModel) {
             gridViewModel.ProcessCustomBinding(
                 MasterCustomBindingHandlers.GetDataRowCount,
                 MasterCustomBindingHandlers.GetData
@@ -39,27 +34,23 @@ namespace Example.Controllers
             return PartialView("MasterGridViewPartial", gridViewModel);
         }
 
-        public ActionResult DetailGridViewPartial(string customerID)
-        {
+        public ActionResult DetailGridViewPartial(string customerID) {
             var viewModel = GridViewExtension.GetViewModel("detailGrid" + customerID);
             if (viewModel == null)
                 viewModel = CreateDetailGridViewModel(customerID);
             return DetailGridActionCore(viewModel, customerID);
         }
-        public ActionResult DetailGridViewPagingAction(GridViewPagerState pager, string customerID)
-        {
+        public ActionResult DetailGridViewPagingAction(GridViewPagerState pager, string customerID) {
             var viewModel = GridViewExtension.GetViewModel("detailGrid" + customerID);
             viewModel.Pager.Assign(pager);
             return DetailGridActionCore(viewModel, customerID);
         }
-        public ActionResult DetailGridViewSortingAction(GridViewColumnState column, bool reset, string customerID)
-        {
+        public ActionResult DetailGridViewSortingAction(GridViewColumnState column, bool reset, string customerID) {
             var viewModel = GridViewExtension.GetViewModel("detailGrid" + customerID);
             viewModel.SortBy(column, reset);
             return DetailGridActionCore(viewModel, customerID);
         }
-        public ActionResult DetailGridActionCore(GridViewModel gridViewModel, string customerID)
-        {
+        public ActionResult DetailGridActionCore(GridViewModel gridViewModel, string customerID) {
             gridViewModel.ProcessCustomBinding(
                 DetailCustomBindingHandlers.GetDataRowCount,
                 DetailCustomBindingHandlers.GetData
@@ -68,8 +59,7 @@ namespace Example.Controllers
             return PartialView("DetailGridViewPartial", gridViewModel);
         }
 
-        static GridViewModel CreateMasterGridViewModel()
-        {
+        static GridViewModel CreateMasterGridViewModel() {
             var viewModel = new GridViewModel();
             viewModel.KeyFieldName = "CustomerID";
             viewModel.Columns.Add("ContactName");
@@ -78,8 +68,7 @@ namespace Example.Controllers
             viewModel.Columns.Add("Country");
             return viewModel;
         }
-        static GridViewModel CreateDetailGridViewModel(string customerID)
-        {
+        static GridViewModel CreateDetailGridViewModel(string customerID) {
             var viewModel = new GridViewModel();
             viewModel.KeyFieldName = "OrderID";
             viewModel.Columns.Add("OrderID");
